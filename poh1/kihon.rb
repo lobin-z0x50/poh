0de1277ead37b49a605be0897bca269774099633
Wataru Maeda <w.maeda@neogenia.co.jp>
2018-01-24 01:37:52 +0900

    POH1 発展系の実装をとりあえず出来たところまでコミット

--- poh1/kihon.rb
+++ poh1/kihon.rb
@@ -2,35 +2,34 @@
 
 $N, $D = gets.split.map &:to_i
 
-def bsearch_idx(arr, target, except_idx, from=0, to=nil)
+def bsearch_idx(arr, target, from=0, to=nil)
   to ||= arr.length-1
-  from += 1 if except_idx == from
-  to -= 1   if except_idx == to
-  STDERR.puts "bsearch_idx arr=#{arr.inspect} target=#{target} from=#{from} to=#{to} except=#{except_idx}"
+  #STDERR.puts "bsearch_idx arr=#{arr.inspect} target=#{target} from=#{from} to=#{to}"
   return nil if to<from
   if to==from
     return arr[from] > target ? nil : from
   end
   p = (from+to)/2
-  p += 1 if except_idx == p
   if arr[p] == target
     return p
   end
   if arr[p] < target
-    return bsearch_idx(arr, target, except_idx, p+1, to)
+    return bsearch_idx(arr, target, p+1, to)
   end
-  bsearch_idx(arr, target, except_idx, from, p-1)
+  bsearch_idx(arr, target, from, p-1)
 end
 
 def search(values, target)
   ans=0
   values.take_while{|v| v<target}.each_with_index do |v1, i|
     limit = target - v1
+    vals = values.dup
+    vals.delete_at(i)
     # limit 以下の値を二分探索で探す
-    idx = bsearch_idx(values, limit, i)
+    idx = bsearch_idx(vals, limit)
     next if idx.nil?
     sum = v1 + values[idx]
-    STDERR.puts "sum=#{sum}"
+    #STDERR.puts "sum=#{sum}"
     ans = sum if sum>ans
   end
   ans
@@ -42,4 +41,3 @@ prices = (1..$N).map{gets.to_i}.sort
 (1..$D).each do
   puts search(prices, gets.to_i)
 end
-
